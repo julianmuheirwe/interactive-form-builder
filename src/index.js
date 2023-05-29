1798f6390c1b24747d3e2c45630833ad96410e1c
julianmuheirwe <julianmuheirwe@gmail.com>
2023-05-29 19:38:54 +0300

    fix render bug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-
 import FormBuilder from "./lib/FormBuilder.vue";
-
 export default {
     install: (app) =>{
       app.component("FormBuilder", FormBuilder)
     },
 };
 
-export {default as FormBuilder} from "./lib/FormBuilder.vue";
+export {default as FormBuilder } from "./lib/FormBuilder.vue";
